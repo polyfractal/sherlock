64cd044c6bdd29f992a1c19df21215aa043e6aa8
Zachary Tong <zacharyjtong@gmail.com>
2013-02-23 11:44:59 -0500

    Fix Indices query to satisfy test

--- src/Sherlock/components/queries/Indices.php
+++ src/Sherlock/components/queries/Indices.php
@@ -21,14 +21,37 @@ class Indices extends \Sherlock\components\BaseComponent implements \Sherlock\co
         parent::__construct($hashMap);
     }
 
+	/**
+	 * @param  array | string $indices,...
+	 * @return Indices
+	 */
+	public function indices($indices)
+	{
+		$args = func_get_args();
+		\Analog\Analog::log("Indicies->Indices(".print_r($args, true).")", \Analog\Analog::DEBUG);
+
+		//single param, array of strings
+		if (count($args) == 1 && is_array($args[0]))
+			$args = $args[0];
+
+		foreach ($args as $arg) {
+			if (is_string($arg))
+				$this->params['indices'][] = $arg;
+		}
+
+		return $this;
+	}
+
+
+
     public function toArray()
     {
         $ret = array (
   'indices' =>
   array (
     'indices' => $this->params["indices"],
-    'query' => $this->params["query"],
-    'no_match_query' => $this->params["no_match_query"],
+    'query' => $this->params["query"]->toArray(),
+    'no_match_query' => $this->params["no_match_query"]->toArray(),
   ),
 );
 
