b0a43658c77e569b4e222986c2c9ea83cba27150
Zachary Tong <zacharyjtong@gmail.com>
2013-02-23 11:53:30 -0500

    Remove fuzzy_rewrite, change lenient to bool

--- src/Sherlock/components/queries/Match.php
+++ src/Sherlock/components/queries/Match.php
@@ -17,8 +17,7 @@ use Sherlock\components;
  * @method \Sherlock\components\queries\Match operator() operator(\string $value) Default: 'and'
  * @method \Sherlock\components\queries\Match analyzer() analyzer(\string $value) Default: 'default'
  * @method \Sherlock\components\queries\Match fuzziness() fuzziness(\float $value) Default: 0.5
- *
- * @method \Sherlock\components\queries\Match lenient() lenient(\int $value) Default: 1
+ * @method \Sherlock\components\queries\Match lenient() lenient(\bool $value) Default: true
  * @method \Sherlock\components\queries\Match max_expansions() max_expansions(\int $value) Default: 100
  * @method \Sherlock\components\queries\Match minimum_should_match() minimum_should_match(\int $value) Default: 2
  * @method \Sherlock\components\queries\Match prefix_length() prefix_length(\int $value) Default: 2
@@ -33,7 +32,7 @@ class Match extends \Sherlock\components\BaseComponent implements \Sherlock\comp
         $this->params['analyzer'] = 'default';
         $this->params['fuzziness'] = 0.5;
         //$this->params['fuzzy_rewrite'] = 'constant_score_default';
-        $this->params['lenient'] = 1;
+        $this->params['lenient'] = true;
         $this->params['max_expansions'] = 100;
         $this->params['minimum_should_match'] = 2;
         $this->params['prefix_length'] = 2;
