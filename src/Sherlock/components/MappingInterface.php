faf2bca6f1820472960cd81b467ff389d1e19e43
Zachary Tong <zacharyjtong@gmail.com>
2013-03-06 08:36:25 -0500

    Docblocks

--- src/Sherlock/components/MappingInterface.php
+++ src/Sherlock/components/MappingInterface.php
@@ -3,10 +3,14 @@
  * User: Zachary Tong
  * Date: 2/16/13
  * Time: 8:14 PM
+ * @package Sherlock\components
  */
 
 namespace Sherlock\components;
 
+/**
+ * Interface for Mapping components, always used in conjunction with the BaseComponent class
+ */
 interface MappingInterface
 {
     public function getType();
