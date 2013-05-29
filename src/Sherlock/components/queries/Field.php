3ab93e8fea164f151f23afbf2b0699c27f3671e4
Zachary Tong <zacharyjtong@gmail.com>
2013-05-29 15:37:40 -0400

    Refactor Field to use static methods
    Modified to extend QueryString so as to reuse code

--- src/Sherlock/components/queries/Field.php
+++ src/Sherlock/components/queries/Field.php
@@ -9,59 +9,63 @@
 namespace Sherlock\components\queries;
 
 use Sherlock\components;
+use Sherlock\components\QueryInterface;
 
 /**
- * @method \Sherlock\components\queries\Field field() field(\string $value)
- * @method \Sherlock\components\queries\Field query() query(\string $value)
- * @method \Sherlock\components\queries\Field boost() boost(\float $value) Default: 2.0
- * @method \Sherlock\components\queries\Field enable_position_increments() enable_position_increments(\int $value) Default: 1
- * @method \Sherlock\components\queries\Field default_operator() default_operator(\string $value) Default: "AND"
- * @method \Sherlock\components\queries\Field analyzer() analyzer(\string $value) Default: "default"
- * @method \Sherlock\components\queries\Field allow_leading_wildcard() allow_leading_wildcard(\bool $value) Default: false
- * @method \Sherlock\components\queries\Field lowercase_expanded_terms() lowercase_expanded_terms(\int $value) Default: 1
- * @method \Sherlock\components\queries\Field fuzzy_min_sim() fuzzy_min_sim(\float $value) Default: 0.5
- * @method \Sherlock\components\queries\Field fuzzy_prefix_length() fuzzy_prefix_length(\int $value) Default: 2
- * @method \Sherlock\components\queries\Field phrase_slop() phrase_slop(\int $value) Default: 10
- * @method \Sherlock\components\queries\Field analyze_wildcard() analyze_wildcard(\bool $value) Default: true
- * @method \Sherlock\components\queries\Field auto_generate_phrase_queries() auto_generate_phrase_queries(\int $value) Default: 0
- * @method \Sherlock\components\queries\Field rewrite() rewrite(\string $value) Default: "constant_score_default"
- * @method \Sherlock\components\queries\Field quote_analyzer() quote_analyzer(\string $value) Default: "standard"
- * @method \Sherlock\components\queries\Field quote_field_suffix() quote_field_suffix(\string $value) Default: ".unstemmed"
-
+ * Class Field
+ * @package Sherlock\components\queries
  */
-class Field extends \Sherlock\components\BaseComponent implements \Sherlock\components\QueryInterface
+class Field extends QueryString implements QueryInterface
 {
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function field($value)
+    {
+        $this->params['field'] = $value;
+        return $this;
+    }
 
 
-
+    /**
+     * @return array
+     */
     public function toArray()
     {
-        $ret = array(
-            'field' =>
+        $this->params[$this->params['field']] = $this->convertParams(
             array(
-                $this->params["field"] =>
-                array(
-                    'query'                        => $this->params["query"],
-                    'boost'                        => $this->params["boost"],
-                    'enable_position_increments'   => $this->params["enable_position_increments"],
-                    'default_operator'             => $this->params["default_operator"],
-                    'analyzer'                     => $this->params["analyzer"],
-                    'allow_leading_wildcard'       => $this->params["allow_leading_wildcard"],
-                    'lowercase_expanded_terms'     => $this->params["lowercase_expanded_terms"],
-                    'fuzzy_min_sim'                => $this->params["fuzzy_min_sim"],
-                    'fuzzy_prefix_length'          => $this->params["fuzzy_prefix_length"],
-                    'phrase_slop'                  => $this->params["phrase_slop"],
-                    'analyze_wildcard'             => $this->params["analyze_wildcard"],
-                    'auto_generate_phrase_queries' => $this->params["auto_generate_phrase_queries"],
-                    'quote_analyzer'               => $this->params["quote_analyzer"],
-                    'quote_field_suffix'           => $this->params["quote_field_suffix"],
-                ),
-                'rewrite'              => $this->params["rewrite"]
-
+                'query',
+                'default_field',
+                'boost',
+                'enable_position_increments',
+                'default_operator',
+                'analyzer',
+                'allow_leading_wildcard',
+                'lowercase_expanded_terms',
+                'fuzzy_min_sim',
+                'fuzzy_prefix_length',
+                'lenient',
+                'phrase_slop',
+                'analyze_wildcard',
+                'auto_generate_phrase_queries',
+                'quote_analyzer',
+                'quote_field_suffix',
             )
         );
 
+        $params = $this->convertParams(
+            array(
+                $this->params['field'],
+                'rewrite',
+            )
+        );
+
+        $ret = array('field' => $params);
+
         return $ret;
+
     }
 
 }
