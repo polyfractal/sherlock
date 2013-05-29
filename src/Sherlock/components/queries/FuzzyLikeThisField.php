e9ebef3555cc7a268ffb16de8c3c11d877f9f3a7
Zachary Tong <zacharyjtong@gmail.com>
2013-05-29 16:03:54 -0400

    Refactor FuzzyLikeThisField to use static methods

--- src/Sherlock/components/queries/FuzzyLikeThisField.php
+++ src/Sherlock/components/queries/FuzzyLikeThisField.php
@@ -9,48 +9,47 @@
 namespace Sherlock\components\queries;
 
 use Sherlock\components;
+use Sherlock\components\QueryInterface;
 
 /**
- * @method \Sherlock\components\queries\FuzzyLikeThisField field() field(\string $value)
- * @method \Sherlock\components\queries\FuzzyLikeThisField like_text() like_text(\string $value)
- * @method \Sherlock\components\queries\FuzzyLikeThisField max_query_terms() max_query_terms(\int $value) Default: 10
- * @method \Sherlock\components\queries\FuzzyLikeThisField min_similarity() min_similarity(\float $value) Default: 0.5
- * @method \Sherlock\components\queries\FuzzyLikeThisField prefix_length() prefix_length(\int $value) Default: 3
- * @method \Sherlock\components\queries\FuzzyLikeThisField boost() boost(\float $value) Default: 2.0
- * @method \Sherlock\components\queries\FuzzyLikeThisField analyzer() analyzer(\string $value) Default: "default"
- * @method \Sherlock\components\queries\FuzzyLikeThisField ignore_tf() ignore_tf(\bool $value) Default: false
-
+ * Class FuzzyLikeThisField
+ * @package Sherlock\components\queries
  */
-class FuzzyLikeThisField extends \Sherlock\components\BaseComponent implements \Sherlock\components\QueryInterface
+class FuzzyLikeThisField extends BaseFuzzyLikeThis implements QueryInterface
 {
-    public function __construct($hashMap = null)
-    {
-        $this->params['max_query_terms'] = 10;
-        $this->params['min_similarity']  = 0.5;
-        $this->params['prefix_length']   = 3;
-        $this->params['boost']           = 2.0;
-        $this->params['analyzer']        = "default";
-        $this->params['ignore_tf']       = false;
 
-        parent::__construct($hashMap);
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function field($value)
+    {
+        $this->params['field'] = $value;
+        return $this;
     }
 
-
+    /**
+     * @return array
+     */
     public function toArray()
     {
-        $ret = array(
-            'fuzzy_like_this_field' =>
+        $params = $this->convertParams(
             array(
-                $this->params["field"] => array(
-                    'like_text'       => $this->params["like_text"],
-                    'max_query_terms' => $this->params["max_query_terms"],
-                    'min_similarity'  => $this->params["min_similarity"],
-                    'prefix_length'   => $this->params["prefix_length"],
-                    'boost'           => $this->params["boost"],
-                    'analyzer'        => $this->params["analyzer"],
-                    'ignore_tf'       => $this->params["ignore_tf"]
-                )
+                'like_text',
+                'max_query_terms',
+                'min_similarity',
+                'prefix_length',
+                'boost',
+                'analyzer',
+                'ignore_tf',
             )
+        );
+
+
+        $ret = array('fuzzy_like_this_field' => array(
+                $this->params["field"] => $params
+            ),
         );
 
         return $ret;
