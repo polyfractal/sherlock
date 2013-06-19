3de5b505d03db3003ead705b570126b1ad969089
Zachary Tong <zacharyjtong@gmail.com>
2013-06-19 17:32:33 -0400

    Update obsolete tests

--- tests/Facades/Search/SearchQueryFacadeTest.php
+++ tests/Facades/Search/SearchQueryFacadeTest.php
@@ -7,201 +7,33 @@
 
 namespace Sherlock\tests\Facades\Search;
 
-
-use Sherlock\Facades\Search\SearchQueryFacade;
-
+use Sherlock\Facades\Search\SearchFacade;
+use Mockery as m;
 
 /**
- * Class SearchQueryFacadeTest
+ * Class SearchFacadeTest
  * @package Sherlock\tests\Facades\SearchWhere
  */
-class SearchQueryFacadeTest extends \PHPUnit_Framework_TestCase
+class SearchFacadeTest extends \PHPUnit_Framework_TestCase
 {
-
-    public function testQueryAndConstructor()
+    public function tearDown()
     {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-            ->method('search')
-            ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo(''),
-                $this->equalTo(''),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-            ->method('toArray')
-            ->will($this->returnValue(array('term' => 'value')));
-
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-            ->method('getSearchResponse');
-
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->get();
+        m::close();
     }
 
-    public function testIndex()
+    public function testQueryReturnsComposer()
     {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-        ->method('search')
-        ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo('testIndex'),
-                $this->equalTo(''),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-        ->method('toArray')
-        ->will($this->returnValue(array('term' => 'value')));
+        $mockTransport = m::mock('\Elasticsearch\Client');
+        $mockResponse  = m::mock('\Sherlock\Responses\ResponseFactory');
+        $mockQuery     = m::mock('\Sherlock\components\queries\MatchAll')
+                         ->shouldReceive('toArray')
+                         ->once()
+                         ->getMock();
 
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-        ->method('getSearchResponse');
+        $facade = new SearchFacade($mockTransport, $mockResponse);
+        $composer = $facade->query($mockQuery);
 
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->index('testIndex');
-
-        $search->get();
+        $this->assertInstanceOf('\Sherlock\Composers\Search\QueryComposer', $composer);
     }
-
-    public function testMultipleIndex()
-    {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-        ->method('search')
-        ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo('testIndex,testIndex2'),
-                $this->equalTo(''),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-        ->method('toArray')
-        ->will($this->returnValue(array('term' => 'value')));
-
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-        ->method('getSearchResponse');
-
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->index('testIndex')->index('testIndex2');
-
-        $search->get();
-    }
-
-    public function testMultipleIndices()
-    {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-        ->method('search')
-        ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo('testIndex,testIndex2'),
-                $this->equalTo(''),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-        ->method('toArray')
-        ->will($this->returnValue(array('term' => 'value')));
-
-
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-        ->method('getSearchResponse');
-
-        $indices = array('testIndex', 'testIndex2');
-
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->indices($indices);
-
-        $search->get();
-    }
-
-
-    public function testType()
-    {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-        ->method('search')
-        ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo(''),
-                $this->equalTo('testType'),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-            ->method('toArray')
-            ->will($this->returnValue(array('term' => 'value')));
-
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-            ->method('getSearchResponse');
-
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->type('testType');
-
-        $search->get();
-    }
-
-    public function testMultipleType()
-    {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-        ->method('search')
-        ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo(''),
-                $this->equalTo('testType,testType2'),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-        ->method('toArray')
-        ->will($this->returnValue(array('term' => 'value')));
-
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-        ->method('getSearchResponse');
-
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->type('testType')->type('testType2');
-
-        $search->get();
-    }
-
-    public function testMultipleTypes()
-    {
-        $mockTransport = $this->getMockBuilder('Sherlock\common\Transport')->disableOriginalConstructor()->getMock();
-        $mockTransport->expects($this->once())
-        ->method('search')
-        ->with(
-                $this->equalTo(array('term' => 'value')),
-                $this->equalTo(''),
-                $this->equalTo('testType,testType2'),
-                $this->isEmpty()
-            );
-        $mockQuery = $this->getMock('Sherlock\components\queries\Term');
-        $mockQuery->expects($this->once())
-        ->method('toArray')
-        ->will($this->returnValue(array('term' => 'value')));
-
-        $mockResponseFactory = $this->getMock('Sherlock\Responses\ResponseFactory');
-        $mockResponseFactory->expects($this->once())
-        ->method('getSearchResponse');
-
-        $types = array('testType', 'testType2');
-        $search = new SearchQueryFacade($mockTransport, $mockResponseFactory, $mockQuery);
-        $search->types($types);
-
-        $search->get();
-    }
-
 
 }
