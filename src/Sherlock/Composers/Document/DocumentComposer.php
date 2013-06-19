9da6de121ad803aea95ab571da87976abb31aa63
Zachary Tong <zacharyjtong@gmail.com>
2013-06-19 13:20:08 -0400

    Split enqueing into separate functions

--- src/Sherlock/Composers/Document/DocumentComposer.php
+++ src/Sherlock/Composers/Document/DocumentComposer.php
@@ -8,6 +8,7 @@
 namespace Sherlock\Composers\Document;
 
 use Elasticsearch\Client;
+use Sherlock\common\exceptions\InvalidArgumentException;
 use Sherlock\facades\Document\DocumentFacade;
 use Sherlock\Responses\ResponseFactory;
 
@@ -17,12 +18,18 @@ use Sherlock\Responses\ResponseFactory;
  */
 class DocumentComposer
 {
+    /** @var \Elasticsearch\Client  */
     private $transport;
 
+    /** @var \Sherlock\Responses\ResponseFactory  */
     private $responseFactory;
 
     /** @var  DocumentFacade */
     private $facade;
+
+    /** @var array  */
+    private $requestQueue = array();
+
 
     /**
      * @param Client          $transport
@@ -46,17 +53,93 @@ class DocumentComposer
 
 
     /**
+     * @param array $request
+     *
+     * @return DocumentFacade
+     */
+    public function enqueueIndex($request)
+    {
+        $this->checkEnqueuedRequest($request);
+        $request = array('index' => $request);
+        return $this->enqueue($request);
+    }
+
+
+    /**
+     * @param array $request
+     *
+     * @return DocumentFacade
+     */
+    public function enqueueDelete($request)
+    {
+        $this->checkEnqueuedRequest($request);
+        $request = array('delete' => $request);
+        return $this->enqueue($request);
+    }
+
+
+    /**
+     * @return array
+     */
+    public function execute()
+    {
+        $responses = array();
+        if (count($this->requestQueue) === 0) {
+            return $responses;
+        }
+
+        foreach ($this->requestQueue as $request) {
+            $responses[] = $this->executeDocumentMethod($request);
+        }
+
+        return $responses;
+    }
+
+
+    /**
+     * @param array $request
+     *
+     * @return array
+     */
+    private function executeDocumentMethod($request)
+    {
+        reset($request);
+        $key   = key($request);
+
+        switch ($key) {
+            case 'index':
+                return $this->transport->index($request);
+
+            case 'delete':
+                return $this->transport->delete($request);
+
+            default:
+                return array();
+        }
+    }
+
+
+    /**
      * @param $request
      *
      * @return DocumentFacade
      */
-    public function enqueue($request)
+    private function enqueue($request)
     {
+        $this->requestQueue[] = $request;
         return $this->facade;
     }
 
-    public function execute()
-    {
 
+    /**
+     * @param array $request
+     *
+     * @throws \Sherlock\common\exceptions\InvalidArgumentException
+     */
+    private function checkEnqueuedRequest($request)
+    {
+        if (is_array($request) !== true || count($request) === 0) {
+            throw new InvalidArgumentException('Cannot enqueue an empty request.');
+        }
     }
 }
